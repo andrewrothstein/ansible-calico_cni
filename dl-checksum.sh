6fde38b4c285bf7d6fad052800bee43d4965d85c
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-03-17 15:55:32 -0400

    up to v3.6.0. dcb v0.0.16. add fedora_29 CI build. delint

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
-VER=v3.2.1
+VER=v3.6.0
 DIR=~/Downloads
 MIRROR=https://github.com/projectcalico/cni-plugin/releases/download/$VER
 
-for PLATFORM in "" -arm64 -ppc64le
+for PLATFORM in amd64 arm64 ppc64le
 do
-    wget -O $DIR/calico$PLATFORM-cni-$VER $MIRROR/calico$PLATFORM
-    wget -O $DIR/calico-ipam$PLATFORM-cni-$VER $MIRROR/calico-ipam$PLATFORM
+    wget -O $DIR/calico-$PLATFORM-$VER $MIRROR/calico-$PLATFORM
+    wget -O $DIR/calico-ipam-$PLATFORM-$VER $MIRROR/calico-ipam-$PLATFORM
 done
-sha256sum $DIR/calico*-cni-$VER
+sha256sum $DIR/calico-*-$VER
 
 
