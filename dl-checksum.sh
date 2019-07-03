2a8b81783392bdf892033af9e32fefff35e418c3
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-07-03 12:27:13 -0500

    up to v3.8.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env sh
-VER=v3.6.0
+VER=v3.8.0
 DIR=~/Downloads
 MIRROR=https://github.com/projectcalico/cni-plugin/releases/download/$VER
 
-for PLATFORM in amd64 arm64 ppc64le
-do
-    wget -O $DIR/calico-$PLATFORM-$VER $MIRROR/calico-$PLATFORM
-    wget -O $DIR/calico-ipam-$PLATFORM-$VER $MIRROR/calico-ipam-$PLATFORM
-done
-sha256sum $DIR/calico-*-$VER
+dl()
+{
+    APP=$1
+    PLATFORM=$2
+    URL=$MIRROR/$APP-$PLATFORM
+    LFILE=$DIR/$APP-$PLATFORM-$VER
+    if [ ! -e $LFILE ]
+    then
+        wget -q -O $LFILE $URL
+    fi
+    printf "        # %s\n" $URL
+    printf "        %s: sha256:%s\n" $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
+}
 
+printf "  %s:\n" $VER
+printf "    - f: calico\n"
+printf "      checksums:\n"
+dl calico amd64
+dl calico arm64
+dl calico ppc64le
 
+printf "    - f: calico-ipam\n"
+printf "      checksums:\n"
+dl calico-ipam amd64
+dl calico-ipam arm64
+dl calico-ipam ppc64le
